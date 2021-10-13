a705c53e520f5080f77e9b94c5e71719d0efd436
PhattOZ <phatto2543@gmail.com>
2021-10-13 22:38:56 +0700

    Create DBconnect.js

---
+++ src/lib/DBconnect.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose"
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define MONGODB_URI environment variable inside .env.local"
+  )
+}
+
+let cache = global.mongoose
+
+if (!cache) {
+  cache = global.mongoose = { conn: null, promise: null }
+}
+
+export default async function dbConnect() {
+  if (cache.conn) {
+    return cache.conn
+  }
+
+  if (!cache.promise) {
+    const opt = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      bufferCommands: false,
+      bufferMaxEntries: 0,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+
+    cache.promise = await mongoose.connect(MONGODB_URI, opt)
+  }
+
+  cache.conn = await cache.promise
+
+  return cache.conn
+}
