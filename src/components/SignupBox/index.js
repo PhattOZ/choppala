7dbc74216b964777c87d23c917cf1dfe875e4d58
Sorawong L <73735636+srwx@users.noreply.github.com>
2021-10-14 09:20:31 +0700

    Added Signup page (#4)

---
+++ src/components/SignupBox/index.js
@@ -0,0 +1,67 @@
+import Link from "next/link"
+import Image from "next/image"
+// Styles
+import styles from "./SignupBox.module.css"
+
+export default function SignupBox() {
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.title}>Create your Choppala Account</h3>
+      <div className={`${styles.childBox} ${styles.inputPart}`}>
+        <label>Email</label>
+        <input
+          className={styles.inputBox}
+          type="text"
+          placeholder="Please enter your email"
+        />
+      </div>
+      <div className={`${styles.childBox} ${styles.inputPart}`}>
+        <label>Password</label>
+        <input
+          className={styles.inputBox}
+          type="password"
+          placeholder="Please enter your password"
+        />
+      </div>
+      <div className={`${styles.childBox} ${styles.checkboxPart}`}>
+        <input type="checkbox" required />
+        <label>
+          I've read and agree to{" "}
+          <Link href="/termsandconditions">
+            <a target="_blank" className={styles.linkText}>
+              Terms & Conditions
+            </a>
+          </Link>
+        </label>
+      </div>
+      <button className={`${styles.childBox} ${styles.signupButton}`}>
+        SIGN UP
+      </button>
+      <div className={styles.orSignupWithBox}>
+        <p className={styles.orSignupWithText}>
+          <span>OR Sign up with</span>
+        </p>
+      </div>
+      <div className={`${styles.childBox} ${styles.oauthButtons}`}>
+        <button className={styles.facebookButton}>
+          <div className={styles.socialIcon}>
+            <Image src="/facebook-white-logo.png" width="11" height="14" />
+          </div>
+          Facebook
+        </button>
+        <button className={styles.googleButton}>
+          <div className={styles.socialIcon}>
+            <Image src="/google-white-logo.png" width="15" height="15" />
+          </div>
+          Google
+        </button>
+      </div>
+      <p className={styles.signinText}>
+        Already have an account?{" "}
+        <Link href="/signin">
+          <a className={styles.linkText}>Log in</a>
+        </Link>
+      </p>
+    </div>
+  )
+}
