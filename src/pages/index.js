6326da0ec1f9220221bbab4a67d02a5def694a5e
Phattarapol L <63938605+PhattOZ@users.noreply.github.com>
2021-10-14 09:15:18 +0700

    Home page layout, Card component, DB connect, Absolute import (#3)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,56 @@
 import Image from "next/image"
+import Card from "src/components/Card"
+import styles from "src/styles/index.module.scss"
 
-export default function Home() {
+function CategoryLink({ category }) {
   return (
-    <>
-      <h1>Home page</h1>
-    </>
+    <div>
+      <div className={styles.category_image}>
+        <Image src="/molang.jpg" layout="fill" objectFit="cover" />
+      </div>
+      <div>{category}</div>
+    </div>
+  )
+}
+
+export default function Index() {
+  const categories = [
+    "Clothes",
+    "Mobiles & Gadgets",
+    "Food & Beverages",
+    "Home Appliances",
+  ]
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <div className={styles.section}>banner</div>
+        <div className={styles.section}>
+          <div className={styles.section_title}>categories</div>
+          <div className={styles.categoriesContainer}>
+            {categories.map((category) => (
+              <CategoryLink key={category} category={category} />
+            ))}
+          </div>
+        </div>
+        <div className={styles.section}>
+          <div className={styles.section_title}>justforyou</div>
+          <div className={styles.cardContainer}>
+            <Card title="haha" price={123} />
+            <Card title="haha" price={123} />
+            <Card title="haha" price={123} />
+            <Card title="haha" price={123} />
+            <Card title="haha" price={123} />
+            <Card title="haha" price={123} />
+            <Card title="haha" price={123} />
+            <Card title="haha" price={123} />
+            <Card title="haha" price={123} />
+            <Card title="haha" price={123} />
+            <Card title="haha" price={123} />
+            <Card title="haha" price={123} />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
