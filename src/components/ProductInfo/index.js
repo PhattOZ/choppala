b12ed512e0985cd9f209a20f98aa41af9622a6e8
kathorn49 <ponypingpong@gmail.com>
2021-10-21 20:51:14 +0700

    Created ProductDetail page

---
+++ src/components/ProductInfo/index.js
@@ -0,0 +1,23 @@
+import styles from "./ProductInfo.module.css"
+
+export default function ProductInfo() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.titlectn}>Product Details</div>
+      <div className={styles.infotxt}>
+        <p>
+          Brand new local set. Receipt will be provided for your warranty
+          registration.Ready Stocks (This is not a Pre-order listing) Digital
+          Edition at 1159 * Free Gift Bundle- Chose Either Gaming Headset Bundle
+          + Goodie Bag or Multi Function Cooling Station Bundle + Goodie BagDisc
+          Edition at 1359 * Free Gift Bundle- Chose Either Gaming Headset Bundle
+          or The Last of Us 2 Special Edition + Random PS4 Game (Hard Copy)Digital
+          Edition at 1159 * Free Gift Bundle- Chose Either Gaming Headset Bundle
+          + Goodie Bag or Multi Function Cooling Station Bundle + Goodie BagDisc
+          Edition at 1359 * Free Gift Bundle- Chose Either Gaming Headset Bundle
+          or The Last of Us 2 Special Edition + Random PS4 Game (Hard Copy)
+        </p>
+      </div>
+    </div>
+  )
+}
