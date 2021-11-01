2ec7a4791d1362b19e02f2addef6dccca405f7df
PhattOZ <phatto2543@gmail.com>
2021-11-01 20:03:26 +0700

    Use noreferrer instead of noopener in rel attribute of a tag

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -109,7 +109,11 @@ export default function Footer() {
             </a>
           </Link>
           <div className={styles.icons}>
-            <a href="https://www.facebook.com/" target="_blank" rel="noopener">
+            <a
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <svg
                 className={styles.icon}
                 xmlns="http://www.w3.org/2000/svg"
@@ -121,7 +125,7 @@ export default function Footer() {
                 <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
               </svg>
             </a>
-            <a href="https://instagram.com/" target="_blank" rel="noopener">
+            <a href="https://instagram.com/" target="_blank" rel="noreferrer">
               <svg
                 className={styles.icon}
                 xmlns="http://www.w3.org/2000/svg"
@@ -136,7 +140,7 @@ export default function Footer() {
             <a
               href="https://twitter.com/?lang=th"
               target="_blank"
-              rel="noopener"
+              rel="noreferrer"
             >
               <svg
                 className={styles.icon}
@@ -168,7 +172,7 @@ export default function Footer() {
                   className={styles.app}
                   href="https://play.google.com/store"
                   target="_blank"
-                  rel="noopener"
+                  rel="noreferrer"
                 >
                   Google Play
                 </a>
@@ -179,7 +183,7 @@ export default function Footer() {
                   className={styles.app}
                   href="https://www.apple.com/th/app-store/"
                   target="_blank"
-                  rel="noopener"
+                  rel="noreferrer"
                 >
                   App Store
                 </a>
