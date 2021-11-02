98f378d068833c5a2e2266ccc9018908bc584008
PhattOZ <phatto2543@gmail.com>
2021-11-02 11:02:38 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,6 @@ module.exports = withPWA({
   },
   pwa: {
     dest: "public",
-    disable: process.env.NODE_ENV === "production" ? false : true,
+    disable: process.env.NODE_ENV === "development",
   },
 })
