e4058ece31690265c90771ea7d64f493f4a38127
PhattOZ <phatto2543@gmail.com>
2021-11-05 10:24:23 +0700

    Subscribe super cool loading spinner to router event

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,15 +2,42 @@ import { SessionProvider } from "next-auth/react"
 import "../styles/globals.css"
 import Layout from "src/components/Layout"
 import { CartContextProvider } from "src/lib/cart-context"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import Loader from "src/components/Loader"
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  const [loading, setLoading] = useState(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = () => {
+      setLoading(true)
+    }
+
+    const handleStop = () => {
+      setLoading(false)
+    }
+
+    router.events.on("routeChangeStart", handleStart)
+    router.events.on("routeChangeComplete", handleStop)
+    router.events.on("routeChangeError", handleStop)
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart)
+      router.events.off("routeChangeComplete", handleStop)
+      router.events.off("routeChangeError", handleStop)
+    }
+  }, [router])
+
   return (
     <SessionProvider session={session}>
       <CartContextProvider>
         <Layout>
+          {loading ? <Loader /> : ""}
           <Component {...pageProps} />
         </Layout>
       </CartContextProvider>
