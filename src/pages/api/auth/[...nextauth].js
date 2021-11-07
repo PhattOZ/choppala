df5ac9983229ac2e945096cdc984fa62f3c7d653
Sorawong L <Sorawong2543@gmail.com>
2021-11-07 23:38:08 +0700

    Fixed error vercel (4)
    Add try-catch block to [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -9,41 +9,45 @@ import LineProvider from "next-auth/providers/line"
 import User from "src/models/User"
 
 export default async function auth(req, res) {
-  return await NextAuth(req, res, {
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      }),
-      FacebookProvider({
-        clientId: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      }),
-      LineProvider({
-        scope: "profile openid email",
-        clientId: process.env.LINE_CLIENT_ID,
-        clientSecret: process.env.LINE_CLIENT_SECRET,
-      }),
-    ],
-    pages: {
-      signIn: "/signup", // ถ้า url เป็น /api/auth/signin ให้ไปที่ localhost:3000/signup
-    },
-    callbacks: {
-      async signIn({ user, account, profile, email, credentials }) {
-        await dbConnect()
-        const checkUser = await User.findOne({
-          name: user.name,
-          email: user.email,
-          image: user.image,
-        })
-        if (checkUser) {
-          return true // user คนนี้มีใน db แล้ว (เคย signin แล้ว)
-        } else {
-          user.provider = account.provider
-          await User.create(user)
-          return true
-        }
+  try {
+    return await NextAuth(req, res, {
+      providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        FacebookProvider({
+          clientId: process.env.FACEBOOK_CLIENT_ID,
+          clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        }),
+        LineProvider({
+          scope: "profile openid email",
+          clientId: process.env.LINE_CLIENT_ID,
+          clientSecret: process.env.LINE_CLIENT_SECRET,
+        }),
+      ],
+      pages: {
+        signIn: "/signup", // ถ้า url เป็น /api/auth/signin ให้ไปที่ localhost:3000/signup
       },
-    },
-  })
+      callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+          await dbConnect()
+          const checkUser = await User.findOne({
+            name: user.name,
+            email: user.email,
+            image: user.image,
+          })
+          if (checkUser) {
+            return true // user คนนี้มีใน db แล้ว (เคย signin แล้ว)
+          } else {
+            user.provider = account.provider
+            await User.create(user)
+            return true
+          }
+        },
+      },
+    })
+  } catch (err) {
+    throw new Error("Error in NextAuth")
+  }
 }
