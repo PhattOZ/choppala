f4904439519a469acb796bf8cec1a51480f06c21
Sorawong L <Sorawong2543@gmail.com>
2021-11-07 23:31:41 +0700

    Fixed vercel error (3)
    Added try-catch block to getServerSideProps in filter.js

--- src/lib/querySearch.js
+++ src/lib/querySearch.js
@@ -9,31 +9,30 @@ export default async function querySearch(
   maxprice,
   sortby
 ) {
-  await dbConnect()
-
-  // ============================ Dynamic query condition ============================
-  const query = {
-    ...(keyword && { name: { $regex: keyword, $options: "i" } }),
-    ...(category && { category }),
-    ...(minprice && { price: { $gte: minprice } }),
-    ...(maxprice && { price: { $lte: maxprice } }),
-    ...(!!minprice &&
-      !!maxprice && {
-        $and: [{ price: { $gte: minprice } }, { price: { $lte: maxprice } }],
-      }),
-  }
-
-  // ================================= Sort condition =================================
-  let itemList = null
   try {
+    await dbConnect()
+
+    // ============================ Dynamic query condition ============================
+    const query = {
+      ...(keyword && { name: { $regex: keyword, $options: "i" } }),
+      ...(category && { category }),
+      ...(minprice && { price: { $gte: minprice } }),
+      ...(maxprice && { price: { $lte: maxprice } }),
+      ...(!!minprice &&
+        !!maxprice && {
+          $and: [{ price: { $gte: minprice } }, { price: { $lte: maxprice } }],
+        }),
+    }
+
+    // ================================= Sort condition =================================
+    let itemList = null
+
     if (sortby === "Latest") {
       itemList = await Item.find(query).sort({ _id: -1 })
     } else if (sortby === "Price low to high") {
       itemList = await Item.find(query).sort({ price: 1 })
     } else if (sortby === "Price high to low") {
       itemList = await Item.find(query).sort({ price: -1 })
-    } else {
-      itemList = await Item.find(query)
     }
     return JSON.parse(JSON.stringify(itemList))
   } catch (err) {
--- src/pages/filter.js
+++ src/pages/filter.js
@@ -111,25 +111,35 @@ export default function Filter({
 }
 
 export async function getServerSideProps(context) {
-  const keyword = context.query.keyword ? context.query.keyword : ""
-  const category = context.query.category ? context.query.category : ""
-  const minprice = context.query.minprice ? context.query.minprice : ""
-  const maxprice = context.query.maxprice ? context.query.maxprice : ""
-  const sortby = context.query.sort ? context.query.sort : "Latest"
-  const data = await querySearch(keyword, category, minprice, maxprice, sortby)
-
-  // Paginate session
-  const page = context.query.page ? context.query.page : "1"
-  const currentItems = spliceData(data, page, itemsPerPage)
-  const totalItems = data.length
-
-  return {
-    props: {
+  try {
+    const keyword = context.query.keyword ? context.query.keyword : ""
+    const category = context.query.category ? context.query.category : ""
+    const minprice = context.query.minprice ? context.query.minprice : ""
+    const maxprice = context.query.maxprice ? context.query.maxprice : ""
+    const sortby = context.query.sort ? context.query.sort : "Latest"
+    const data = await querySearch(
       keyword,
       category,
-      currentItems,
-      sortby,
-      totalItems,
-    },
+      minprice,
+      maxprice,
+      sortby
+    )
+
+    // Paginate session
+    const page = context.query.page ? context.query.page : "1"
+    const currentItems = spliceData(data, page, itemsPerPage)
+    const totalItems = data.length
+
+    return {
+      props: {
+        keyword,
+        category,
+        currentItems,
+        sortby,
+        totalItems,
+      },
+    }
+  } catch (err) {
+    throw new Error("Error in getServerSideProps in filter.js")
   }
 }
