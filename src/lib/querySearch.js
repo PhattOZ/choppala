a33450b812fdbfd3c4e57b20d23a4d42e2aafe4e
Sorawong L <Sorawong2543@gmail.com>
2021-11-08 20:49:57 +0700

    Sort by dropdown
    Removed LINE OAuth

--- src/lib/querySearch.js
+++ src/lib/querySearch.js
@@ -1,23 +1,38 @@
 import Item from "src/models/Item"
 import dbConnect from "./dbConnect"
+import { sortConditions } from "./sortTitles"
 
 export default async function querySearch(
   keyword,
   category,
-  minPrice,
-  maxPrice
+  minprice,
+  maxprice,
+  sortby
 ) {
   await dbConnect()
+
+  // ============================ Dynamic query condition ============================
   const query = {
     ...(keyword && { name: { $regex: keyword, $options: "i" } }),
     ...(category && { category }),
-    ...(minPrice && { price: { $gte: minPrice } }),
-    ...(maxPrice && { price: { $lte: maxPrice } }),
-    ...(!!minPrice &&
-      !!maxPrice && {
-        $and: [{ price: { $gte: minPrice } }, { price: { $lte: maxPrice } }],
+    ...(minprice && { price: { $gte: minprice } }),
+    ...(maxprice && { price: { $lte: maxprice } }),
+    ...(!!minprice &&
+      !!maxprice && {
+        $and: [{ price: { $gte: minprice } }, { price: { $lte: maxprice } }],
       }),
   }
-  const itemList = await Item.find(query)
+
+  // ================================= Sort condition =================================
+  let sort = null
+  if (sortby === "Latest") {
+    sort = { _id: -1 }
+  } else if (sortby === "Price low to high") {
+    sort = { price: 1 }
+  } else if (sortby === "Price high to low") {
+    sort = { price: -1 }
+  }
+
+  const itemList = await Item.find(query).sort(sort)
   return JSON.parse(JSON.stringify(itemList))
 }
