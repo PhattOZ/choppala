6771680f0deb2dbdd222d49ecc41d1b54578925c
Phattarapol L <63938605+PhattOZ@users.noreply.github.com>
2021-11-12 17:30:47 +0700

    Optimize loading indicator,layout and landing page (#37)

--- src/components/UserProfileLayout/index.js
+++ src/components/UserProfileLayout/index.js
@@ -10,6 +10,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons"
 import Image from "next/image"
 import Link from "next/link"
+import Head from "next/head"
 import styles from "./UserProfileLayout.module.scss"
 import { signOut } from "next-auth/react"
 import { useRouter } from "next/router"
@@ -32,60 +33,77 @@ function SidebarItem({ children, content, path }) {
   )
 }
 
+function Sidebar({ user }) {
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.sidebar_header}>
+        <div className={styles.sidebar_user_img}>
+          <Image
+            src={user.image}
+            layout="fill"
+            objectFit="cover"
+            alt=""
+            userimage
+          />
+        </div>
+        <div>
+          <div className={styles.bold}>{user.name}</div>
+          <div className={styles.email}>{user.email}</div>
+        </div>
+      </div>
+      <ul>
+        <ul className={styles.sidebar_list}>
+          <SidebarItem content="Profile" path="/me">
+            <FontAwesomeIcon icon={faUser} size={"lg"} />
+          </SidebarItem>
+
+          <SidebarItem content="Purchase history" path="/me/purchasehistory">
+            <FontAwesomeIcon icon={faHistory} size={"lg"} />
+          </SidebarItem>
+
+          <SidebarItem content="Wishlist" path="/me/wishlist">
+            <FontAwesomeIcon icon={faHeart} size={"lg"} />
+          </SidebarItem>
+        </ul>
+        <ul className={styles.sidebar_list}>
+          <SidebarItem content="Manage store" path="/me/managestore">
+            <FontAwesomeIcon icon={faStore} size={"lg"} />
+          </SidebarItem>
+
+          <SidebarItem content="Selling orders" path="/me/sellingorders">
+            <FontAwesomeIcon icon={faTags} size={"lg"} />
+          </SidebarItem>
+        </ul>
+        <ul className={styles.sidebar_list}>
+          <SidebarItem content="Setting" path="/me/setting">
+            <FontAwesomeIcon icon={faCog} size={"lg"} />
+          </SidebarItem>
+
+          <li
+            className={`${styles.sidebar_list_item} ${styles.signout}`}
+            onClick={() => signOut({ callbackUrl: "/" })}
+          >
+            <span>
+              <FontAwesomeIcon icon={faSignOutAlt} size={"lg"} />
+            </span>
+            <div>Logout</div>
+          </li>
+        </ul>
+      </ul>
+    </div>
+  )
+}
+
 export default function Layout({ children, user }) {
   return (
-    <div className={styles.container}>
-      <div className={styles.sidebar}>
-        <div className={styles.sidebar_header}>
-          <div className={styles.sidebar_user_img}>
-            <Image src={user.image} layout="fill" objectFit="cover" alt=""userimage/>
-          </div>
-          <div>
-            <div className={styles.bold}>{user.name}</div>
-            <div className={styles.email}>{user.email}</div>
-          </div>
-        </div>
-        <ul>
-          <ul className={styles.sidebar_list}>
-            <SidebarItem content="Profile" path="/me">
-              <FontAwesomeIcon icon={faUser} size={"lg"} />
-            </SidebarItem>
-
-            <SidebarItem content="Purchase history" path="/me/purchasehistory">
-              <FontAwesomeIcon icon={faHistory} size={"lg"} />
-            </SidebarItem>
-
-            <SidebarItem content="Wishlist" path="/me/wishlist">
-              <FontAwesomeIcon icon={faHeart} size={"lg"} />
-            </SidebarItem>
-          </ul>
-          <ul className={styles.sidebar_list}>
-            <SidebarItem content="Manage store" path="/me/managestore">
-              <FontAwesomeIcon icon={faStore} size={"lg"} />
-            </SidebarItem>
-
-            <SidebarItem content="Selling orders" path="/me/sellingorders">
-              <FontAwesomeIcon icon={faTags} size={"lg"} />
-            </SidebarItem>
-          </ul>
-          <ul className={styles.sidebar_list}>
-            <SidebarItem content="Setting" path="/me/setting">
-              <FontAwesomeIcon icon={faCog} size={"lg"} />
-            </SidebarItem>
-
-            <li
-              className={`${styles.sidebar_list_item} ${styles.signout}`}
-              onClick={() => signOut({ callbackUrl: "/" })}
-            >
-              <span>
-                <FontAwesomeIcon icon={faSignOutAlt} size={"lg"} />
-              </span>
-              <div>Logout</div>
-            </li>
-          </ul>
-        </ul>
+    <>
+      <Head>
+        <title>{`${user.name}'s`} Profile | Choppala</title>
+      </Head>
+      <div className={styles.container}>
+        <Sidebar user={user} />
+        {children}
       </div>
-      {children}
-    </div>
+    </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
+import Head from "next/head"
 import Card from "src/components/Card"
 import categories from "src/lib/categoryList"
 import styles from "src/styles/pages/index.module.scss"
@@ -8,70 +9,81 @@ import Item from "src/models/Item"
 
 function CategoryLink({ category }) {
   return (
-    <div className={styles.category}>
-      <div className={styles.category_image}>
-        <Image
-          src="/molang.jpg"
-          layout="fill"
-          objectFit="cover"
-          alt="category"
-        />
-      </div>
-      <div className={styles.category_title}>{category}</div>
-    </div>
+    <Link href={{ pathname: "/filter", query: { category } }}>
+      <a>
+        <div className={styles.category}>
+          <div className={styles.category_image}>
+            <Image
+              src="/molang.jpg"
+              layout="fill"
+              objectFit="cover"
+              alt="category"
+            />
+          </div>
+          <div className={styles.category_title}>{category}</div>
+        </div>
+      </a>
+    </Link>
   )
 }
 
 export default function Index({ productList }) {
   return (
-    <div className={styles.container}>
-      <div>
-        <div className={styles.section}>banner</div>
-        <div className={styles.section}>
-          <div className={styles.section_title}>Categories</div>
-          <div
-            className={styles.categoriesContainer}
-            style={{ "--category-length": categories.length }}
-          >
-            {categories.map((category) => (
-              <Link
-                key={category}
-                href={{ pathname: "/filter", query: { category } }}
-              >
-                <a>
-                  <CategoryLink category={category} />
-                </a>
-              </Link>
-            ))}
-          </div>
-        </div>
-        <div className={styles.section}>
-          <div className={styles.section_title}>Just for you</div>
-          <div className={styles.cardContainer}>
-            {productList.map((product) => (
-              <Link key={product._id} href={`/product/${product._id}`}>
-                <a>
-                  <Card
-                    title={product.name}
-                    price={product.price}
-                    image={product.image}
-                  />
-                </a>
-              </Link>
-            ))}
-          </div>
+    <>
+      <Head>
+        <title>Choppala | Shop Chim & Chill</title>
+        <meta
+          name="description"
+          content="Wonderously designed ecommerse application. Choppala is crafted carefully and delicately utilizing topics learned during Covid lockdown."
+        />
+      </Head>
+      <div className={styles.container}>
+        <div>
+          <div className={styles.section}>banner</div>
+          <section className={styles.section}>
+            <div className={styles.section_title}>Categories</div>
+            <div
+              className={styles.categoriesContainer}
+              style={{ "--category-length": categories.length }}
+            >
+              {categories.map((category) => (
+                <CategoryLink key={category} category={category} />
+              ))}
+            </div>
+          </section>
+          <section className={styles.section}>
+            <div className={styles.section_title}>Just for you</div>
+            <div className={styles.cardContainer}>
+              {productList.map((product) => (
+                <Link key={product._id} href={`/product/${product._id}`}>
+                  <a>
+                    <Card
+                      title={product.name}
+                      price={product.price}
+                      image={product.image}
+                    />
+                  </a>
+                </Link>
+              ))}
+            </div>
+          </section>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps() {
   await dbConnect()
-  const productList = await Item.find().sort({ _id: -1 }).limit(18)
+  const items = await Item.find({}).sort({ _id: -1 }).limit(18).lean()
+
+  items.map((item) => {
+    item._id = item._id.toString()
+  })
+
   return {
     props: {
-      productList: JSON.parse(JSON.stringify(productList)),
+      productList: items,
     },
   }
 }
