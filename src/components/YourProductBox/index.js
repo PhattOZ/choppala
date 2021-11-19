c3fe045203367f2393bd1a2214a76043ad91134b
Sorawong L <Sorawong2543@gmail.com>
2021-11-19 22:18:39 +0700

    Refactor managestore.js
    - Using only <YourProductBox /> to handle seller item. (If seller never add item, render FirstProduct component, otherwise render YourProductBox component)

--- src/components/YourProductBox/index.js
+++ src/components/YourProductBox/index.js
@@ -5,14 +5,15 @@ import styles from "./YourProductBox.module.scss"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons"
 import { faCoins, faTrash } from "@fortawesome/free-solid-svg-icons"
+import FirstProduct from "../FirstProduct"
 
-function SellingBox() {
+function SellingBox({ name, image, price, amount, sold }) {
   return (
     <div className={styles.orderBox}>
       <div className={styles.flexInfo}>
         <div className={styles.img}>
           <Image
-            src="/molang2.jpg"
+            src={image}
             alt="Product image"
             layout="fill"
             objectFit="cover"
@@ -20,21 +21,19 @@ function SellingBox() {
         </div>
         <div className={styles.info}>
           <div className={styles.nameblock}>
-            <div className={styles.textInfo}>
-              Sony vegus Pro 16 Sony vegus Pro 16 Sony vegus Pro 16
-            </div>
+            <div className={styles.textInfo}>{name}</div>
           </div>
           <div className={styles.infoblock}>
             <div className={styles.priceblock}>
               <FontAwesomeIcon icon={faCoins} size="sm" color="#FFD44D" />
-              <div className={styles.textInfo}>1299</div>
+              <div className={styles.textInfo}>{price}</div>
             </div>
           </div>
           <div className={styles.infoblock}>
-            <div className={styles.textInfo}>100</div>
+            <div className={styles.textInfo}>{amount}</div>
           </div>
           <div className={styles.infoblock}>
-            <div className={styles.textInfo}>9</div>
+            <div className={styles.textInfo}>{sold}</div>
           </div>
           <div className={styles.btnblock}>
             <div className={styles.deleteBtn}>
@@ -48,37 +47,50 @@ function SellingBox() {
   )
 }
 
-export default function YourProduct() {
+export default function YourProductBox({ sellerItems }) {
   return (
     <>
-      <div className={styles.main}>
-        <section>
-          <div className={styles.title}>Your Product</div>
-          <div className={styles.body}>
-            <div className={styles.header}>
-              <div className={styles.subtitle}>Product Name</div>
-              <div></div>
-              <div className={styles.subheader}>
-                <div className={styles.subtitle}>Price/Unit</div>
-                <div className={styles.subtitle}>Amount</div>
-                <div className={styles.subtitle}>Sold</div>
+      {sellerItems.length ? (
+        <div className={styles.main}>
+          <section>
+            <div className={styles.title}>Your Product</div>
+            <div className={styles.body}>
+              <div className={styles.header}>
+                <div className={styles.subtitle}>Product Name</div>
+                <div></div>
+                <div className={styles.subheader}>
+                  <div className={styles.subtitle}>Price/Unit</div>
+                  <div className={styles.subtitle}>Amount</div>
+                  <div className={styles.subtitle}>Sold</div>
+                </div>
+                <div className={styles.subheader}></div>
               </div>
-              <div className={styles.subheader}></div>
-            </div>
 
-            <SellingBox />
-            <SellingBox />
-          </div>
-          <div className={styles.button_wrapper}>
-            <Link href="/me/addproduct" passHref>
-              <div className={styles.addBtn}>
-                <FontAwesomeIcon icon={faPlusCircle} size="lg" />
-                Add product
-              </div>
-            </Link>
-          </div>
-        </section>
-      </div>
+              {sellerItems.map((item) => (
+                <SellingBox
+                  name={item.name}
+                  image={item.images[0]}
+                  price={item.price}
+                  amount={item.amount}
+                  sold={item.soldCount}
+                />
+              ))}
+            </div>
+            <div className={styles.button_wrapper}>
+              <Link href="/me/addproduct" passHref>
+                <div className={styles.addBtn}>
+                  <FontAwesomeIcon icon={faPlusCircle} size="lg" />
+                  Add product
+                </div>
+              </Link>
+            </div>
+          </section>
+        </div>
+      ) : (
+        <>
+          <FirstProduct />
+        </>
+      )}
     </>
   )
 }
--- src/pages/me/sellingorders.js
+++ src/pages/me/sellingorders.js
@@ -1,18 +1,20 @@
 import Layout from "src/components/UserProfileLayout"
 import styles from "src/styles/pages/user/SellingOrders.module.scss"
-import FirstProduct from "src/components/FirstProduct"
 import ActivateSeller from "src/components/ActivateSeller"
 import YourProduct from "src/components/YourProductBox"
 import { getSession } from "next-auth/react"
 import dbConnect from "src/lib/dbConnect"
 import User from "src/models/User"
+import Seller from "src/models/Seller"
+import Item from "src/models/Item"
+import YourProductBox from "src/components/YourProductBox"
 
-export default function SellingOrders({ user }) {
+export default function SellingOrders({ user, sellerItems }) {
   return (
     <div className={styles.container}>
       <Layout user={user}>
         {user.isSeller ? (
-          <FirstProduct />
+          <YourProductBox sellerItems={sellerItems} />
         ) : (
           <ActivateSeller
             userId={user._id}
@@ -38,9 +40,20 @@ export async function getServerSideProps(context) {
 
     leanResponse._id = leanResponse._id.toString()
 
+    const sellerLeanResponse = await Seller.findOne(
+      { userId: leanResponse._id },
+      { _id: 0 }
+    ).lean()
+
+    const sellerItems = await Item.find(
+      { sellerId: sellerLeanResponse.id },
+      { _id: 0 }
+    ).lean()
+
     return {
       props: {
         user: leanResponse,
+        sellerItems,
       },
     }
   } else {
