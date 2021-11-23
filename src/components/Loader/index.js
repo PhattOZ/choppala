056717c950d27f2b925c81f7017fbc1f28290cda
PhattOZ <63938605+PhattOZ@users.noreply.github.com>
2021-11-23 16:00:57 +0700

    [Upgraded] Loading indicator

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,15 +1,36 @@
+import ReactDOM from "react-dom"
 import Image from "next/image"
 import styles from "./Loader.module.scss"
+import { useLayoutEffect } from "react"
 
 export default function Loader() {
-  return (
-    <div className={styles.body}>
-      <div className={styles.loader}>
-        <div className={styles.spinner}></div>
-        <div className={styles.logo}>
-          <Image src="/molang.jpg" layout="fill" objectFit="contain" />
+  useLockBodyScroll()
+
+  return ReactDOM.createPortal(
+    <>
+      <div className={styles.body}>
+        <div className={styles.loader}>
+          <div className={styles.spinner}></div>
+          <div className={styles.logo}>
+            <Image
+              src="/molang.jpg"
+              layout="fill"
+              objectFit="contain"
+              priority
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>,
+    document.getElementById("modal-root")
   )
+}
+
+function useLockBodyScroll() {
+  useLayoutEffect(() => {
+    //Prevent scrolling on mount
+    document.body.style.overflow = "hidden"
+    //Re-enable scrolling when component unmount
+    return () => (document.body.style.overflow = "visible")
+  }, []) // Empty array ensures effect is only run on mount and unmount
 }
