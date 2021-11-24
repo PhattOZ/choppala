7cbe827921a37859c2c008e8aed1d93f2a624c8c
Phattarapol L <63938605+PhattOZ@users.noreply.github.com>
2021-11-24 10:29:19 +0700

    [Upgraded] Loading indicator (#44)

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,15 +1,44 @@
+import { createPortal } from "react-dom"
 import Image from "next/image"
 import styles from "./Loader.module.scss"
+import { useLayoutEffect, useState } from "react"
 
 export default function Loader() {
-  return (
-    <div className={styles.body}>
-      <div className={styles.loader}>
-        <div className={styles.spinner}></div>
-        <div className={styles.logo}>
-          <Image src="/molang.jpg" layout="fill" objectFit="contain" />
-        </div>
-      </div>
-    </div>
-  )
+  const [mounted, setMounted] = useState(false)
+
+  useLayoutEffect(() => {
+    setMounted(true)
+
+    return () => setMounted(false)
+  }, [])
+
+  useLockBodyScroll()
+
+  return mounted
+    ? createPortal(
+        <div className={styles.body}>
+          <div className={styles.loader}>
+            <div className={styles.spinner}></div>
+            <div className={styles.logo}>
+              <Image
+                src="/molang.jpg"
+                layout="fill"
+                objectFit="contain"
+                priority
+              />
+            </div>
+          </div>
+        </div>,
+        document.getElementById("modal-root")
+      )
+    : null
+}
+
+function useLockBodyScroll() {
+  useLayoutEffect(() => {
+    //Prevent scrolling on mount
+    document.body.style.overflow = "hidden"
+    //Re-enable scrolling when component unmount
+    return () => (document.body.style.overflow = "visible")
+  }, []) // Empty array ensures effect is only run on mount and unmount
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,7 +19,7 @@ export default function App({
     const handleStart = () => {
       id = setInterval(() => {
         setLoading(true)
-      }, 500)
+      }, 250)
     }
 
     const handleStop = () => {
