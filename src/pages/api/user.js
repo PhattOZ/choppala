39d4e9a56065baabe504869eedb55ac0c868bb61
Sorawong L <Sorawong2543@gmail.com>
2021-11-28 13:59:14 +0700

    Create user DELETE API

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,6 +1,8 @@
 // Database
 import dbConnect from "src/lib/dbConnect"
 import User from "src/models/User"
+import Item from "src/models/Item"
+import Seller from "src/models/Seller"
 // mongoose lib
 import mongoose from "mongoose"
 
@@ -14,6 +16,22 @@ export default async function handler(req, res) {
         const id = mongoose.Types.ObjectId(userId)
         await dbConnect()
         await User.findByIdAndUpdate(id, { ...data })
+        res.status(200).json({ success: true })
+      } catch (err) {
+        res.status(400).json({ success: false })
+      }
+      break
+    case "DELETE":
+      try {
+        const userId = req.query.userId
+        const { sellerId } = req.body
+        await dbConnect()
+        const id = mongoose.Types.ObjectId(userId)
+        await User.findByIdAndDelete(id)
+        if (!!sellerId) {
+          await Item.deleteMany({ sellerId })
+          await Seller.findOneAndDelete({ id: sellerId })
+        }
         res.status(200).json({ success: true })
       } catch (err) {
         res.status(400).json({ success: false })
