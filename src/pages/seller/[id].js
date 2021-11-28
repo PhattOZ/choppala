3e97d6beff0b24b452b6e36d9d311173f7bac05a
Phakathorn P <90753674+kathorn49@users.noreply.github.com>
2021-11-28 15:21:48 +0700

    Seller UI, Small Popup (#57)
    * Seller UI, Small Popup
    
    * Fix Single quotes
    
    * Sample
    
    * Fixed

--- src/pages/seller/[id].js
+++ src/pages/seller/[id].js
@@ -5,8 +5,60 @@ import Image from "next/image"
 // Database
 import dbConnect from "src/lib/dbConnect"
 import Seller from "src/models/Seller"
+import Item from "src/models/Item"
 // Style
 import styles from "src/styles/pages/storeProfile.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons"
+//Component
+import Card from "src/components/Card"
+
+function Topbar({ seller }) {
+  return (
+    <div className={styles.topbar}>
+      <div className={styles.topbar_header}>
+        <div className={styles.cover_container}>
+          <div className={styles.cover}>
+            <Image
+              src={seller.storeImage}
+              layout="fill"
+              objectFit="cover"
+              alt="StoreImage"
+            />
+          </div>
+        </div>
+
+        <div className={styles.img_profile}>
+          <div className={styles.storeImg}>
+            <Image
+              src={seller.storeImage}
+              layout="fill"
+              objectFit="cover"
+              alt="StoreImage"
+            />
+          </div>
+        </div>
+        <div className={styles.profile}>
+          <div className={styles.profile_info}>
+            <div className={styles.title}>
+              {seller.storeName}&#39;s store
+              <div className={styles.check_line}>
+                <FontAwesomeIcon icon={faCheckCircle} />
+                seller
+              </div>
+            </div>
+            <div className={styles.email_line}>{seller.storeEmail}</div>
+          </div>
+
+          <div className={styles.item_info}>
+            <div className={styles.subtitle}>No. of items</div>
+            <div className={styles.subtitle}>2</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default function SellerProfile({ seller }) {
   const [sellerItems, setSellerItems] = useState([])
@@ -21,35 +73,36 @@ export default function SellerProfile({ seller }) {
 
   return (
     <>
-      {/* Store profile session*/}
-      <div>
-        <h3>Store profile</h3>
-        <div className={styles.storeImg}>
-          <Image
-            src={seller.storeImage}
-            layout="fill"
-            objectFit="cover"
-            alt="StoreImage"
-          />
+      <div className={styles.container}>
+        {/* Store profile session*/}
+        <Topbar seller={seller} />
+        <div className={styles.main}>
+          <section>
+            <div className={styles.section_title}>
+              All products
+            </div>
+            {/* Store itemlist session */}
+            <div>
+              {itemLoading ? (
+                <div className={styles.subtitle}>Loading...</div>
+              ) : (
+                <div>
+                  <div className={styles.cardContainer}>
+                    {sellerItems.map((item) => (
+                          <Card
+                            key={item.id}
+                            itemID={item.id}
+                            title={item.name}
+                            price={item.price}
+                            image={item.images[0]}
+                          />
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          </section>
         </div>
-        <div>Store name : {seller.storeName}</div>
-        <div>Store email : {seller.storeEmail}</div>
-      </div>
-      <hr />
-      {/* Store itemlist session */}
-      <div>
-        {itemLoading ? (
-          <h1>Loading...</h1>
-        ) : (
-          <div>
-            <h3>Store items</h3>
-            {sellerItems.map((i) => (
-              <Link href={`/product/${i.id}`} key={i.id}>
-                <div>{i.name}</div>
-              </Link>
-            ))}
-          </div>
-        )}
       </div>
     </>
   )
@@ -59,6 +112,8 @@ export async function getServerSideProps(context) {
   const { id } = context.query
   await dbConnect()
   const data = await Seller.findById(id, { _id: 0 }).lean()
+  const items = await Item.find({}, { _id: 0 }).lean()
+
   return {
     props: {
       seller: data,
